a4323c631c90f8668c6b4ed519f34269ed878ec0
cartwrld <62627257+cartwrld@users.noreply.github.com>
2023-06-03 22:05:19 -0600

    looking gooooooooooooooood

--- gamscripts.js
+++ gamscripts.js
@@ -43,6 +43,7 @@ let marker;
 
 $(function () {
     
+    estNavBar()
     estLocationList()
     
     loadMap(locationList[0]).then(
@@ -62,36 +63,113 @@ $(function () {
     $(ptc).on('click', function () {
         catName = $(this).text().trim();
         
-        for (let i=0; i<ptc.length; i++) {
-        
-        }
-        
-        let icon = $(this).children();
         let opened = 'bi-caret-down-fill';
         let closed = 'bi-caret-right-fill';
         
-        if ($(icon).hasClass(closed)) {
-            $(icon).removeClass(closed);
-            $(icon).addClass(opened);
-        } else if ($(this).children().hasClass(opened)) {
-            $(this).children().removeClass(opened);
-            $(this).children().addClass(closed);
-        } else {
-            console.log("fail")
-            
+        let icon;
+        
+        for (let i=0; i<ptc.length; i++) {
+            $(ptc[i]).removeClass()
+            icon = $(ptc[i]).children();
+            $(icon).removeClass(opened)
+            $(icon).addClass(closed)
         }
         
-        $(ptc).not(this).each(function () {
-            $($(this).attr('href')).collapse('hide');
-           
-            // $(icon).removeClass(opened);
-            // $(icon).addClass(closed);
+        icon = $(this).children();
+        $(icon).removeClass(closed)
+        $(icon).addClass(opened)
         
+        $(ptc).not(this).each(function() {
+            
+            $($(this).attr('href')).collapse('hide');
+            
+            
+            
         });
+      
+        
+        // if ($(icon).hasClass(closed)) {
+        //     $(icon).removeClass(closed);
+        //     $(icon).addClass(opened);
+        // } else
+        //     if ($(this).children().hasClass(opened)) {
+        //     $(this).children().removeClass(opened);
+        //     $(this).children().addClass(closed);
+        // } else {
+        //     // console.log("fail")
+        //
+      
+        //
+        // }
+        //     setTimeout()
+        // $(icon).removeClass(closed);
+        // $(icon).addClass(opened);
+        //
+        
+        
     });
     handleProductTreeItemClick();
     buildBreadCrumbTrail()
 });
+
+function estNavBar() {
+    $('#navbarHTML').html(`
+    <nav class="navbar navbar-expand-lg bg-success navbar-dark sticky-top">
+    <div class="container-fluid ">
+
+        <div class="container-fluid text-center d-flex flex-row flex wrap  align-items-center">
+            <a href="easteregg.html">
+                <img src="media/GAM_logo.png" alt="GAM Logo" style="width:135px;">
+            </a>
+            <div class="container-fluid ">
+                <div class="navbar-text text-center h1 h6-xl">Great Asian Market</div>
+            </div>
+            <button class="navbar-toggler mt-xl-3" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="container-fluid collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav container h5">
+                <li class="text-center text-light d-flex flex-row justify-content-center align-items-center">
+                    <a class="nav-link" href="home.html">Home</a>
+                </li>
+                <li class="mt-2 px-2">
+                    <span class="nav-separator d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">|</span>
+                </li>
+                <li class="nav-item text-center d-flex flex-row justify-content-center align-items-center">
+                    <a class="nav-link" href="promotions.html">Promotions</a>
+                </li>
+                <li class="mt-2 px-2">
+                    <span class="nav-separator d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">|</span>
+                </li>
+                <li class="nav-item text-center d-flex flex-row justify-content-center align-items-center">
+                    <a class="nav-link" href="productcatalog.html">Product Catalog</a>
+                </li>
+                <li class="mt-2 px-2">
+                    <span class="nav-separator d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">|</span>
+                </li>
+                <li class="nav-item text-center d-flex flex-row justify-content-center align-items-center">
+                    <a class="nav-link" href="franchise.html">Franchise Opportunities</a>
+                </li>
+                <li class="mt-2 px-2">
+                    <span class="nav-separator d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">|</span>
+                </li>
+                <li class="nav-item text-center d-flex flex-row justify-content-center align-items-center">
+                    <a class="nav-link" id="contactUsNavItem" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+    `);
+}
+
+
+
+
+
 
 function loadAllProducts() {
     for (let i = 0; i < 36; i++) {
